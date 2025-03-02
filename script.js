2147c4630798b40ae3934c728de83e19f43a1557
kwamebuah <66295784+kwamebuah@users.noreply.github.com>
2025-03-02 18:53:44 +0000

    Make function to display book info on card
    Make a seperate function that displays the info on the books stored in
    the array.

--- script.js
+++ script.js
@@ -19,3 +19,18 @@ addBookToLibrary('All Systems Red', 'Martha Wells', 400);
 addBookToLibrary('Harry Potter and the Chamber of Secrets', 'J.K. Rowling', 450);
 
 // console.log(myLibrary);
+function displayBook() {
+    for (const book of myLibrary) {
+        const card = document.createElement('section');
+        card.classList.add('book-card');
+        for (const key in book) {
+            const para = document.createElement('p');
+            para.textContent = `${key}: ${book[key]}`;
+            card.appendChild(para);
+        }
+        const body = document.querySelector('body');
+        body.appendChild(card);
+    }
+}
+
+displayBook();
