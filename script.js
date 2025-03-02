03610a43c917b7c1f4c3b52c33ff24fbd45acb71
kwamebuah <66295784+kwamebuah@users.noreply.github.com>
2025-03-02 18:43:01 +0000

    Set up files for library app
    Create a basic file framework for the project.
    Define constructor to create a new book.
    Make function to create books and store in array to organize and
    seperate operations.

---
+++ script.js
@@ -0,0 +1,22 @@
+const myLibrary = [];
+
+function Book(title, author, numOfPages = 1) {
+    this['Title'] = title;
+    this['Author'] = author;
+    this['Number of Pages'] = numOfPages;
+}
+
+function addBookToLibrary(title, author, numOfPages = 1) {
+    for (let i = myLibrary.length; i >= myLibrary.length; i--) {
+        const newBook = new Book(title, author, numOfPages);
+        myLibrary.push(newBook);
+    }
+}
+
+addBookToLibrary('The Hobbit', 'J.R.R Tolkien', 295);
+addBookToLibrary('The Fellowship of the Rings', 'J.R.R Tolkien', 457);
+addBookToLibrary('All Systems Red', 'Martha Wells', 400);
+
+// console.log(myLibrary);
+
+
