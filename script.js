335fe957d4f57614b8136f02a3dfe4ca1d497cbe
kwamebuah <66295784+kwamebuah@users.noreply.github.com>
2025-03-02 19:07:21 +0000

    Add whether book has been read or not

--- script.js
+++ script.js
@@ -1,21 +1,22 @@
 const myLibrary = [];
 
-function Book(title, author, numOfPages = 1) {
+function Book(title, author, numOfPages = 1, isRead='No') {
     this['Title'] = title;
     this['Author'] = author;
     this['Number of Pages'] = numOfPages;
+    this['Have You Read It'] = isRead;
 }
 
-function addBookToLibrary(title, author, numOfPages = 1) {
+function addBookToLibrary(title, author, numOfPages, isRead) {
     for (let i = myLibrary.length; i >= myLibrary.length; i--) {
-        const newBook = new Book(title, author, numOfPages);
+        const newBook = new Book(title, author, numOfPages, isRead);
         myLibrary.push(newBook);
     }
 }
 
-addBookToLibrary('The Hobbit', 'J.R.R Tolkien', 295);
-addBookToLibrary('The Fellowship of the Rings', 'J.R.R Tolkien', 457);
-addBookToLibrary('All Systems Red', 'Martha Wells', 400);
+addBookToLibrary('The Hobbit', 'J.R.R Tolkien', 295, 'yes');
+addBookToLibrary('The Fellowship of the Rings', 'J.R.R Tolkien', 457, 'yes');
+addBookToLibrary('All Systems Red', 'Martha Wells', 400, 'yes');
 addBookToLibrary('Harry Potter and the Chamber of Secrets', 'J.K. Rowling', 450);
 
 // console.log(myLibrary);
